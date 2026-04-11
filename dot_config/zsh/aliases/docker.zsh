2c54a1f4ba8ecd9a698afbe1bbfa99934568ee89
Germain Masse <g-dev@masse.me>
2026-04-11 17:16:54 -0400

    Initial commit

---
+++ dot_config/zsh/aliases/docker.zsh
@@ -0,0 +1,7 @@
+# ── Docker Compose ─────────────────────────────────────
+alias dc='docker compose'
+alias dcu='docker compose up'
+alias dcd='docker compose down'
+alias dcps='docker compose ps'
+alias dclog='docker compose logs -f'
+alias dcex='docker compose exec'
---
+++ dot_config/zsh/aliases/general.zsh
@@ -0,0 +1,18 @@
+
+alias mv='mv -i'                   # Ask for confirmation
+alias cp='cp -i'                   # Ask for confirmation
+alias ln='ln -i'                   # Ask for confirmation
+alias ll='ls -lah'                 # Use a long listing format
+alias la='ls -A'                   # List all except . and ..
+alias l='ls -CF'                   # Compact listing
+alias du='du -h'                   # Human-readable disk usage
+alias df='df -h'                   # Human-readable filesystem usage
+alias mtr='mtr -z --displaymode=1' # Display stripchart and AS numbers
+
+alias ls='ls --color=auto'         # Colorize the ls output
+alias diff='diff --color=auto'     # Colorize the diff output
+alias egrep='egrep --color=auto'   # Colorize the grep output
+alias fgrep='fgrep --color=auto'   # Colorize the grep output
+alias grep='grep --color=auto'     # Colorize the grep output
+alias ip='ip --color=auto'         # Colorize the ip output
+alias less='less -R'               # Preserve colored output
